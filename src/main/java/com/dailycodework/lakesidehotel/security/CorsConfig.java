0bd58d058f471b7c2cd1979e5b9b2d229150a19d
thanawin597-2 <thanawin.p@kkumail.com>
2025-10-08 04:45:51 +0700

    Update CORS and env config

--- src/main/java/com/dailycodework/lakesidehotel/security/CorsConfig.java
+++ src/main/java/com/dailycodework/lakesidehotel/security/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.dailycodework.lakesidehotel.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +13,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Arrays;
 
-/**
- * @author Simpson Alfred
- */
-
 @Configuration
 @EnableWebMvc
 public class CorsConfig {
@@ -23,24 +20,38 @@ public class CorsConfig {
     private static final Long MAX_AGE = 3600L;
     private static final int CORS_FILTER_ORDER = -102;
 
+    
+    @Value("${FRONTEND_URL:http://localhost:5173}") 
+    private String frontendUrl; 
+
     @Bean
-    public FilterRegistrationBean corsFilter() {
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
+
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173", // dev
+                frontendUrl             // production
+        ));
+
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
-                HttpHeaders.ACCEPT));
+                HttpHeaders.ACCEPT
+        ));
+
         config.setAllowedMethods(Arrays.asList(
                 HttpMethod.GET.name(),
                 HttpMethod.POST.name(),
                 HttpMethod.PUT.name(),
-                HttpMethod.DELETE.name()));
+                HttpMethod.DELETE.name()
+        ));
+
         config.setMaxAge(MAX_AGE);
         source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
         bean.setOrder(CORS_FILTER_ORDER);
         return bean;
     }
